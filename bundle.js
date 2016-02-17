25177cea88fc01f32888617bd59a110b2862104f
ToddKranenburg <todd.kranenburg@gmail.com>
2016-02-17 18:09:29 -0500

    Modify portfolio link.

--- bundle.js
+++ bundle.js
@@ -87,7 +87,7 @@
 	          ),
 	          React.createElement(
 	            'a',
-	            { href: 'http://toddkranenburg.com', className: 'my-link' },
+	            { href: '/', className: 'my-link' },
 	            React.createElement('img', { className: 'my-image', src: './images/portfolio-icon.png' })
 	          )
 	        )
